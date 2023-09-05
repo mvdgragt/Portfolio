08f7ffde2c5cbabfbb70322c20fe1bcebbc18ef9
mvdgragt <michiel@vandergragt.eu>
2023-09-05 15:16:20 +0200

    added more sections

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,21 @@
 import HeroSection from "../HeroSection";
 
 import React from 'react'
+import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
+import MyPortfolio from "../MyPortfolio";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
 
 function Home() {
   return (
 <>
 <HeroSection />
+<MySkills />
+<AboutMe />
+<MyPortfolio />
+<ContactMe />
+<Footer />
 </>  )
 }
 
