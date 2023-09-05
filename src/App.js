5fd34d98e74382f100fa779925580e28d0109730
mvdgragt <michiel@vandergragt.eu>
2023-09-05 12:52:48 +0200

    working on HeroSection profile picture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home/Homescreen';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Pages/Home/Navbar";
+import Home from "./Pages/Home/Homescreen";
 
 function App() {
   return (
